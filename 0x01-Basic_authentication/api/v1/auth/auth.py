6443447715f738b1505583d2dd0fdb67081542b6
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-15 21:56:17 +0000

    routes determined

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,19 @@ class Auth:
         """
         Auth required method
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path == excluded_paths:
+            return False
+        if not path.endswith("/"):
+            path += "/"
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/"):
+                if path.startswith(excluded_path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
