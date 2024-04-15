c9479a429bc90456ff3ccd6c4a880a1fd9397fcd
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-15 23:17:54 +0000

    base64 decoded

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """
+        A method that returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_size = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_size.decode('utf-8')
+            return decoded_str
+        except Exception as err:
+            return None
