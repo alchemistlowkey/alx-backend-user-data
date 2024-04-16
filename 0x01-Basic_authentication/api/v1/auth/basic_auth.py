7776e5610664be5b18eea07a59bec25f99e5ed71
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-16 04:09:33 +0000

    User credentials authenticated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,3 +38,18 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception as err:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        A method that returns the user email and password
+        from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
