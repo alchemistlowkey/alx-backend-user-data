6b4d09f7b8dda3d094e19e68f6ae56e9dc11e0ad
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-23 21:21:59 +0000

    password hassed

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,56 @@
+from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hash a password using bcrypt
+
+        Args:
+            password (str): The password to hash
+
+        Returns:
+            bytes: Salted hash of the input password
+        """
+
+        salted = gensalt()
+
+        hashed_input_password = hashpw(password.encode('utf-8'), salted)
+
+        return hashed_input_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user
+
+        Args:
+            email (str): Email of the user
+            password (str): Password of the user
+
+        Returns:
+            User: The newly registered User object
+
+        Raises:
+            ValueError: If a user already exists with the provided email
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password = self._hash_password(password)
+
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+
+        return user
