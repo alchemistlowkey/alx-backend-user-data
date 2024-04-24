1c96e75496b862abd2434b5d20189c2bcb6b224f
alchemistlowkey <alchemistlowkey@gmail.com>
2024-04-24 05:36:04 +0000

    user registered

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Flask App
+"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """
+    A flask app that has a single get route
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    The end-point to register a user
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
